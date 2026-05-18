810a845707feaae6778aba5f8c89a06c34f3c793
Jakub Fudali <jakubfudali14@gmail.com>
2026-05-18 21:48:58 +0200

    feat: setup monorepo with basic angular, spring boot rest api and latex document for project

---
+++ apps/eth-transactions-fuzzy-classifier-api/src/main/java/com/jfudali/ethtransactionsfuzzyclassifierapi/EthTransactionsFuzzyClassifierApiApplication.java
@@ -0,0 +1,13 @@
+package com.jfudali.ethtransactionsfuzzyclassifierapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthTransactionsFuzzyClassifierApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthTransactionsFuzzyClassifierApiApplication.class, args);
+	}
+
+}
---
+++ apps/eth-transactions-fuzzy-classifier-api/src/test/java/com/jfudali/ethtransactionsfuzzyclassifierapi/EthTransactionsFuzzyClassifierApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfudali.ethtransactionsfuzzyclassifierapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthTransactionsFuzzyClassifierApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
